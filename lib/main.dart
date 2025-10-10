6132d6b1fea3a203a7cbd45fbd57ad6de379725e
George Busby <gbusby01@protonmail.com>
2025-10-10 15:22:21 +0100

    remove counter, change main text

--- lib/main.dart
+++ lib/main.dart
@@ -54,8 +54,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
   void _incrementCounter() {
     setState(() {
       // This call to setState tells the Flutter framework that something has
@@ -63,7 +61,6 @@ class _MyHomePageState extends State<MyHomePage> {
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
-      _counter++;
     });
   }
 
@@ -104,10 +101,12 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              'Welcome to my shop!',
+              style: TextStyle(
+                fontSize: 40,
+                color: Theme.of(context).colorScheme.inversePrimary,
+              ),
             ),
           ],
         ),
